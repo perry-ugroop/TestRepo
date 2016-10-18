b9514a3c9e0c6606b0e557ec56fca708fc5dec14
Perry Valdez <perry.valdez@ugroop.com.au>
2016-10-18 20:00:17 +0700

    Created model for general ledger accounts

---
+++ src/BasicAcct/BasicAcct.Lib/General/AccountingElementType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicAcct.Lib.General
+{
+    public enum AccountingElementType
+    {
+        Asset,
+        Liability,
+        Capital,
+        Revenue,
+        Expense
+    }
+}
---
+++ src/BasicAcct/BasicAcct.Lib/Models/GeneralLedgerAccount.cs
@@ -0,0 +1,17 @@
+﻿using BasicAcct.Lib.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicAcct.Lib.Models
+{
+    public class GeneralLedgerAccount
+    {
+        public int ID { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public AccountingElementType ElementType { get; set; }
+    }
+}
